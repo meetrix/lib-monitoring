00e85f7e000ec2c131d2dd841e779074858304e3
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2021-01-05 20:34:11 +1000

    Import webrtc stats

--- src/lib-call-quality-monitoring.ts
+++ src/lib-call-quality-monitoring.ts
@@ -1,6 +1,16 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export default class DummyClass {
 
+import WebrtcStats from '@peermetrics/webrtc-stats';
+import { MonitoringConstructorOptions } from './types';
+export default class DummyClass {
+  backendUrl: string;
+
+  constructor( { backendUrl }: MonitoringConstructorOptions){
+    this.backendUrl = backendUrl;
+    const webRTCstats = new WebrtcStats({
+      getStatsInterval: 5000
+  })
+  }
 }
