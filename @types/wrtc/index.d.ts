7d828e4657838b0305cb8d390235f439735b05a9
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2021-01-09 22:27:31 +1000

    Attempt to use types for wrtc

---
+++ @types/wrtc/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="webrtc" />
+
+declare module 'wrtc' {
+
+}
--- src/lib-call-quality-monitoring.test.ts
+++ src/lib-call-quality-monitoring.test.ts
@@ -1,14 +1,23 @@
-import Monitor from "./lib-call-quality-monitoring"
+import { RTCPeerConnection } from 'wrtc'
+import Monitor from './lib-call-quality-monitoring'
 
 /**
  * Dummy test
  */
-describe("Monitor Test", () => {
-  it("works if true is truthy", () => {
+describe('Monitor Test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it("Monitor is instantiable", () => {
-    expect(new Monitor({ backendUrl: 'https://meetrix.io'})).toBeInstanceOf(Monitor)
+  it('Monitor is instantiable', () => {
+    expect(new Monitor({ backendUrl: 'https://meetrix.io' })).toBeInstanceOf(Monitor)
+  })
+
+  it('Add peer to Monitor', () => {
+    const monitor = new Monitor({ backendUrl: 'https://meetrix.io' })
+    monitor.addPeer({
+      pc: new RTCPeerConnection(),
+      peerId: '1234'
+    })
   })
 })
