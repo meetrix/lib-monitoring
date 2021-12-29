713de6042f29aa0121167c25bb90db68d7867cce
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2021-12-29 21:24:43 +1000

    Chnage plugin space to client space

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 type Envoronments = 'development' | 'staging' | 'production'
 const environment = process.env.NODE_ENV as Envoronments
 const BACKEND_URLS = {
-  development: 'http://localhost:9100/plugins',
-  staging: 'https://stats.dev.meetrix.io/plugins',
-  production: 'https://stats.meetrix.io/plugins'
+  development: 'http://localhost:9100/clients',
+  staging: 'https://stats.dev.meetrix.io/clients',
+  production: 'https://stats.meetrix.io/clients'
 }
 
 export const BACKEND_URL = BACKEND_URLS[environment]
