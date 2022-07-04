79abef22ee76fc8dd01df05a0cbd60819702c118
Wickramaranga Abeygunawardhana <wickramaranga@meetrix.io>
2022-07-04 09:46:25 +0530

    Fix styling

--- src/ui/components/SetupModal.tsx
+++ src/ui/components/SetupModal.tsx
@@ -3,11 +3,6 @@ import { createStyles, WithStyles, withStyles } from '@mui/styles';
 
 const styles = (theme: Theme) => {
   return createStyles({
-    formHeading: {
-      marginBottom: '2vh',
-      fontSize: '1.8rem',
-      fontWeight: 'bolder',
-    },
     root: {
       width: 'clamp(550px ,40vw, 700px)',
       margin: 'auto',
@@ -20,7 +15,27 @@ const styles = (theme: Theme) => {
     bottomWrapper: {
       display: 'flex',
       justifyContent: 'space-between',
-      alignItems: 'flex-start',
+      alignItems: 'flex-end',
+    },
+    textField: {
+      marginTop: '0.8rem',
+      width: '100%',
+      color: '#5f5f5f8a',
+      fontSize: '1rem',
+      '& .MuiOutlinedInput-input': {
+        fontSize: '1rem',
+        color: '#5f5f5f8a',
+        padding: '5px 7px',
+      },
+      '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+        borderColor: '#DAE3FA',
+      },
+      '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+        borderColor: '#DAE3FA',
+      },
+      '&:hover': {
+        borderColor: '#DAE3FA',
+      },
     },
   });
 };
@@ -43,15 +58,27 @@ const SetupModal = (props: SetupModalProps) => {
       aria-describedby="test-modal-description"
     >
       <Box className={props.classes.contentWrapper}>
-        <div className={props.classes.formHeading}>Enter your email</div>
+        <Typography id="setup-modal-title" variant="h6" color="darkslategray">
+          Enter your email
+        </Typography>
+        <Typography
+          id="setup-modal-description"
+          variant="caption"
+          marginTop="0.8rem"
+          marginBottom="0.8rem"
+          color="darkgray"
+        >
+          Please do not close this window until the test completes
+        </Typography>
         <TextField
+          className={props.classes.textField}
           type="email"
           id="test-modal-title"
           value={props.email}
           onChange={ev => props.onEmailChange(ev.target.value)}
         />
         <div className={props.classes.bottomWrapper}>
-          <Button disabled={!props.email.includes('@')} onClick={props.onStart}>
+          <Button variant='contained' disableElevation disabled={!props.email.includes('@')} onClick={props.onStart}>
             {props.startLabel}
           </Button>
         </div>
