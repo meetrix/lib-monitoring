876b9de2607e4ad0ab93c8235f3119c85bd67c6b
Supuni-Jayasinghe <supuni.18@cse.mrt.ac.lk>
2022-07-05 14:41:56 +0530

    Change client ID modal styles and add close button to the modal

--- src/ui/components/SetupModal.tsx
+++ src/ui/components/SetupModal.tsx
@@ -1,5 +1,16 @@
-import { Box, Button, TextField, Modal, ModalProps, Theme, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { 
+  Box, 
+  Button, 
+  TextField, 
+  Modal, 
+  ModalProps, 
+  Theme, 
+  Typography, 
+  IconButton 
+} from '@mui/material';
 import { createStyles, WithStyles, withStyles } from '@mui/styles';
+import CloseIcon from '@mui/icons-material/Close';
 
 const styles = (theme: Theme) => {
   return createStyles({
@@ -9,21 +20,20 @@ const styles = (theme: Theme) => {
     },
     contentWrapper: {
       backgroundColor: '#fff',
-      padding: '3vw',
+      padding: '40px',
       borderRadius: 5,
     },
     bottomWrapper: {
       display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
     },
     textField: {
       marginTop: '0.8rem',
       width: '100%',
       color: '#5f5f5f8a',
-      fontSize: '1rem',
+      fontSize: '0.875rem',
       '& .MuiOutlinedInput-input': {
-        fontSize: '1rem',
+        fontSize: '0.875rem',
         color: '#5f5f5f8a',
         padding: '5px 7px',
       },
@@ -33,9 +43,27 @@ const styles = (theme: Theme) => {
       '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
         borderColor: '#DAE3FA',
       },
-      '&:hover': {
-        borderColor: '#DAE3FA',
+      '&:hover .MuiOutlinedInput-notchedOutline': {
+        borderColor: '#DAE3FA !important',
       },
+    },
+    modalTitle: {
+      color: '#5F5F5F',
+      paddingBottom: '10px',
+    },
+    startButton: {
+      textTransform: 'capitalize !important' as 'capitalize',
+      '&.MuiButton-root': {
+        backgroundColor: '#4A74E9',
+      },
+      '& .MuiButton-root.Mui-disabled': {
+        backgroundColor: '#0000001f !important',
+      },
+    },
+    closeButton: {
+      position: 'absolute !important' as 'absolute',
+      top: 0,
+      right: 0,
     },
   });
 };
@@ -49,36 +77,50 @@ interface SetupModalProps extends WithStyles<ModalProps & typeof styles> {
 }
 
 const SetupModal = (props: SetupModalProps) => {
+  const [closeModal, setCloseModal] = useState<boolean>(props.open);
   return (
     <Modal
-      open={props.open}
+      open={closeModal}
       className={props.classes.root}
       disableEnforceFocus
       aria-labelledby="test-modal-title"
       aria-describedby="test-modal-description"
     >
       <Box className={props.classes.contentWrapper}>
-        <Typography id="setup-modal-title" variant="h6" color="darkslategray">
-          Enter your email
+        <IconButton 
+          aria-label="close" 
+          className={props.classes.closeButton}
+          onClick={() => setCloseModal(false)}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+        <Typography id="setup-modal-title" variant="h5" className={props.classes.modalTitle}>
+          Enter Email ID
         </Typography>
         <Typography
           id="setup-modal-description"
-          variant="caption"
-          marginTop="0.8rem"
-          marginBottom="0.8rem"
-          color="darkgray"
+          variant="body1"
+          color="#A9A9A9"
         >
           Please do not close this window until the test completes
         </Typography>
-        <TextField
-          className={props.classes.textField}
-          type="email"
-          id="test-modal-title"
-          value={props.email}
-          onChange={ev => props.onEmailChange(ev.target.value)}
-        />
+        <div style={{padding:'10px 0px 20px'}}>
+          <TextField
+            className={props.classes.textField}
+            type="email"
+            id="test-modal-title"
+            value={props.email}
+            onChange={ev => props.onEmailChange(ev.target.value)}
+          />
+        </div>
         <div className={props.classes.bottomWrapper}>
-          <Button variant='contained' disableElevation disabled={!props.email.includes('@')} onClick={props.onStart}>
+          <Button 
+            variant='contained' 
+            disableElevation 
+            disabled={!props.email.includes('@')} 
+            onClick={props.onStart} 
+            className={props.classes.startButton}
+          >
             {props.startLabel}
           </Button>
         </div>
